cb019c0ecc7852b78033a7fecb4f5dce2f16e31c
mdparvej <m.parvej68@gmail.com>
2025-10-16 07:56:46 +0600

    one

--- api/server.js
+++ api/server.js
@@ -1,10 +1,28 @@
 
+const express = require('express');
 const serverless = require('serverless-http');
 
-const app = require('../index');
-// Middleware
+const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('🎉 Hello from Serverless Express App!');
+});
+
+app.get('/api/user/:name', (req, res) => {
+  const { name } = req.params;
+  res.json({ message: `Welcome, ${name}! 👋` });
+});
+
+app.post('/api/data', (req, res) => {
+  const { data } = req.body;
+  res.json({ received: data, status: 'success ✅' });
+});
 
 // Export both app & handler
 module.exports = app;
 module.exports.handler = serverless(app);
+
