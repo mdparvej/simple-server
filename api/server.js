c2e6b562dc5a1c96082ef720c0409a1c70557a7a
mdparvej <m.parvej68@gmail.com>
2025-10-16 08:33:34 +0600

    next

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ const serverless = require('serverless-http');
 
 const app = require('../index');
 
-
+app.get('/parvej', (req, res) => {
+  res.send('🎉 Hello from Serverless Express App!');
+});
 // Export both app & handler
 module.exports = app;
 module.exports.handler = serverless(app);
